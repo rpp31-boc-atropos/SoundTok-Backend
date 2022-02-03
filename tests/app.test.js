126198a9820b31a651431ab32f1b10d4f4284e80
Rick Kunz <rick.c.kunz@gmail.com>
2022-02-03 13:07:38 -0800

    Installed ESLint

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,9 @@ const request = require('supertest');
 describe('Example Jest Test', () => {
   test('1+1=2', () => {
     expect(1+1).toBe(2);
-  })
+    console.log('test');
+
+  });
 });
 
 describe('Server responds to endpoints', () => {
