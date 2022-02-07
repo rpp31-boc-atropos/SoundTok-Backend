995804ea67ffde36946e32b8da3a44f0feee74be
Maggie Saldivia <Maggiesaldivia@gmail.com>
2022-02-06 23:10:44 -0500

    Update index.js

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'soundtok',
+  database: process.env.DB_NAME || 'soundTok',
   password: process.env.DB_PASS || '',
   port: process.env.DB_PORT || 5432
 });
