b1bd0225e22ce0188d61527278b4e5b4a8d0e8c7
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-20 14:42:11 -0500

    refactor postPosts

--- database/userPosts.js
+++ database/userPosts.js
@@ -31,18 +31,18 @@ const getPosts = async (req, res) => {
 
 const postPost = async (req, res) => {
   const { userId, timePosted, username, postLikes, postSaved, postText, tags, projectAudioLink, projectTitle, projectImageLink, projectLength } = req.body;
-  const query1 = "INSERT INTO posts (timePosted, postText, tags, user_id) VALUES ($1, $2, $3, (SELECT id FROM user_accounts WHERE username = $4) ) RETURNING *"
-  const query2 = "INSERT INTO projects (projecttitle, projectlength, projectImage, post_id, user_id) VALUES ($1, $2, $3, (SELECT max(id) FROM posts), $4 ) RETURNING *"
+  const query1 = "INSERT INTO posts (timePosted, postLikes, postSaved, postText, projectAudioLink, projectTitle, projectImageLink, projectLength, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, (SELECT id FROM user_accounts WHERE username = $9) ) RETURNING *"
+  const query2 = "INSERT INTO hashtags (hashtagArr, post_id) VALUES ($1, (SELECT max(id) FROM posts) ) RETURNING *"
   await pool
-    .query(query1, [timePosted, postText, tags, username])
+    .query(query1, [timePosted, postLikes, postSaved, postText, projectAudioLink, projectTitle, projectImageLink, projectLength, username])
     .then(results => {
-      console.log('insert into projects table complete');
+      console.log('insert into posts table complete');
     })
     .then(() => {
-      pool.query(query2, [projectTitle, projectLength, projectImageLink, userId])
+      pool.query(query2, [tags])
     })
     .then(results => {
-      console.log('insert into posts table complete')
+      console.log('insert into hashtags table complete')
       res.status(201).json('inserts complete!')
     })
     .catch(err => {
