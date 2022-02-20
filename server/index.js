50ccb4d4027d6eec1e09f79025022af602b28b7d
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-20 17:46:46 -0500

    need to merge userAccounts to test authO

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pool = require('../database/config.js');
+const userAccounts = require('../database/userAccounts.js');
 const userPosts = require('../database/userPosts.js');
 const userProfile = require('../database/userProfile.js');
 const port = 1234;
@@ -22,7 +23,7 @@ app.get('/', userPosts.getPosts);
 app.post('/', userPosts.postPost);
 app.put('/', userPosts.updateLikes);
 app.put('/', userPosts.updateSave);
-
+app.post('/createUser', userAccounts.createUser);
 app.get('/userProjects', userProfile.getUserProjects);
 
 
