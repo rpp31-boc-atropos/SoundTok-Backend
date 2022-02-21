e8d06b0e7f559c197b1d93de0ea1fb2eba948704
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-20 20:19:37 -0500

    added query for creating users

--- database/userAccounts.js
+++ database/userAccounts.js
@@ -1,22 +1,28 @@
 const pool = require('../database/config.js');
 const express = require('express');
 
-
 const createUser = async (req, res) => {
-  console.log('in here!');
-  console.log(req.body);
   const { email, username, name, nickname, picture } = req.body.params;
-  const query1 = "INSERT INTO user_accounts (email, username, profilePicture) VALUES ($1, $2, $3) RETURNING *"
-  const params = [email, username, picture];
-  await pool
-    .query(query1, params)
-    .then(results => {
-      console.log(`user information for ${name} created!`);
-      res.status(201).json('added a user to the database');
+  const query1 = "SELECT email FROM user_accounts WHERE email = $1"
+  const params1 = [email]
+  const query2 = "INSERT INTO user_accounts (email, username, profilePicture) VALUES ($1, $2, $3) RETURNING *"
+  const params2 = [email, username, picture];
+
+  try {
+    const data = await pool.query(query1, params1); // check if user is in DB
+    if (data.rows.length !== 0) {
+      res.status(200).json(`${username} is an existing user`);
+    } else {
+      console.log(`${username} added to the database!`);
+      res.status(201).json(`${username} added to the database`)
+      await pool.query(query2, params2)
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Database error"
     })
-    .catch(err => {
-      console.log(err.stack)
-    })
+  }
 }
 
 
