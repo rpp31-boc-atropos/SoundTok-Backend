c1a3f90617ce7aa1f026a8886a9f895dd11efb5d
Clayton Watterson <cwatterson@gmail.com>
2022-02-22 23:44:20 -0500

    Drafts endpoints

---
+++ database/userDrafts.js
@@ -0,0 +1,45 @@
+const pool = require('../database/config.js');
+const express = require('express');
+
+// get all drafts for provided username
+const getUserDrafts = async (req, res) => {
+  const { username } = req.params;
+
+  await pool
+    .query(
+      `
+      SElECT
+        d.name AS name,
+        d.date AS date,
+        d.tracks AS tracks
+      FROM user_accounts u
+      JOIN drafts d ON u.id = d.user_id
+      WHERE u.username = $1
+      GROUP BY u.id
+      `, [username]
+    )
+    .then(results => {
+      res.status(200).json(results.rows)
+    })
+    .catch(err => console.log('error executing query', err.stack))
+};
+
+const postDraft = async (req, res) => {
+  const { username, name, date, tracks } = req.body;
+  const query1 = "INSERT INTO drafts (user_id, name, date, tracks) VALUES ((SELECT id FROM user_accounts WHERE username = $1), $2, $3, $4) RETURNING *";
+  await pool
+    .query(query1, [username, name, date, tracks])
+    .then(results => {
+      console.log(`inserted ${name} into drafts table complete`);
+      res.status(201).json(`inserted ${name} into drafts table`)
+    })
+    .catch(err => {
+      console.log(err.stack)
+    })
+};
+
+
+module.exports = {
+  getUserDrafts,
+  postDraft
+};
