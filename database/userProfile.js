5009b35b5da7fb6af82b884acd20bf2f41b70d78
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-24 17:47:04 -0500

    fix bug

--- database/userProfile.js
+++ database/userProfile.js
@@ -40,7 +40,7 @@ const updateProfile = async (req, res) => {
 
   const { username, profilePicture, bio } = req.body;
   pool
-    .query(`UPDATE user_accounts SET profilePicture = $1`, [profileUrl])
+    .query(`UPDATE user_accounts SET profilePicture = $1`, [profilePicture])
     .then(result => {
       console.log(`${username}: profile picture updated`)
       res.status(201)
