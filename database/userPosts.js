991357e612f9915925c2074de639307c79265853
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-24 23:26:13 -0500

    adding console logs to debug

--- database/userPosts.js
+++ database/userPosts.js
@@ -34,6 +34,8 @@ const getPosts = async (req, res) => {
 
 const postPost = async (req, res) => {
   //console.log(req.body)
+  console.log(req)
+  console.log('in post post')
   let { published, tracks, userId, timePosted, username, postLikes, postText, tags, projectAudioLink, projectTitle, projectImageLink, projectLength } = req.body;
   const query1 = "INSERT INTO posts (published, timePosted, postLikes, postText, projectAudioLink, projectTitle, projectImageLink, projectLength, user_id, tracks) VALUES (true, $1, $2, $3, $4, $5, $6, $7, (SELECT id FROM user_accounts WHERE username = $8), $9 ) RETURNING *"
   const query2 = "INSERT INTO hashtags (hashtagArr, post_id) VALUES ($1, (SELECT max(id) FROM posts) ) RETURNING *"
