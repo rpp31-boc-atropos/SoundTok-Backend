58289c90baf3a76b5fc2c5d63ec458cc341c67b0
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-28 20:39:41 -0500

    adding endpoint test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,8 +1,20 @@
-//const app = require('../server/index.js');
+const app = require('../server/index.js');
 const request = require('supertest');
+const requestWithSupertest = request(app);
 
 describe('Example Jest Test', () => {
   test('1+1=2', () => {
     expect(1 + 1).toBe(2);
   });
+});
+
+describe('Feed Endpoints', () => {
+
+  it('GET / should show all posts', async () => {
+    const res = await requestWithSupertest.get('/');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+
+  });
+
 });
